7af167e345855fb31e6e7e5ae94327abfdbd4906
fabiodrneles <fabiodrneles@gmail.com>
2024-11-29 13:43:40 -0300

    finalizando teste de busca de produto

--- src/test/java/Home.java
+++ src/test/java/Home.java
@@ -1,6 +1,9 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
 public class Home {
+
+    private WebDriver driver;
 
     // Campo de pesquisa
     By campoPesquisa = By.cssSelector("#blog > div.page-wrap > div.site-header.container-fluid.woo-heading > div > div > div.header-search-widget > div.head-form.hidden-xs > div > form > input.header-search-input");
@@ -9,13 +12,26 @@ public class Home {
     By botaoPesquisa = By.cssSelector("#blog > div.page-wrap > div.site-header.container-fluid.woo-heading > div > div > div.header-search-widget > div.head-form.hidden-xs > div > form > button");
 
     // Inserir o nome do produto que eu quero pesquisar
-    public void insereNomeCampoPesquisa () {
-
+    public void insereNomeCampoPesquisa (String nomeDoProdutoParaBuscar) {
+        driver.findElement(campoPesquisa).sendKeys(nomeDoProdutoParaBuscar);
     }
 
 
     // Clicar no botão pesquisa para realizar a ação
     public void clicaBotaoPesquisar () {
+        driver.findElement(botaoPesquisa).click();
+    }
 
+    //Não inserir nada no campo de pesquisa
+    public void naoInsereNadaNaBarraDePesquisa () {
+        driver.findElement(campoPesquisa).sendKeys("");
+    }
+
+
+
+
+
+    public Home(WebDriver driver) {
+        this.driver = driver;
     }
 }
--- src/test/java/TestaPaginas.java
+++ src/test/java/TestaPaginas.java
@@ -5,11 +5,14 @@ import org.openqa.selenium.devtools.v129.page.model.WebAppManifest;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestaPaginas {
 
     private WebDriver driver;
     private static final String baseURL = "https://automacao.testerglobal.com/";
+    Home homePage;
 
 
 
@@ -18,23 +21,50 @@ public class TestaPaginas {
     public void pesquisarCampoVazio () {
 
         // Entrar na página de testes
+        driver.get(baseURL);
+
+        homePage = new Home(driver);
 
         // Não inseriu nada no campo de pesquisa
+        homePage.naoInsereNadaNaBarraDePesquisa();
 
         // Clicar no botão pesquisa
+        homePage.clicaBotaoPesquisar();
 
 
     }
 
-    //Método para preparação, tudo o que irá rodar antes dos testes.
+    @Test
+    @DisplayName("Digita camera na barra de pesquisa de produto e depois clica em pesquisar. Abre a pagina do produto com os detalhes do produto.")
+    public void pesquisarProduto () {
+        // Entrar na página de testes
+        driver.get(baseURL);
+        homePage = new Home(driver);
+
+        //Insere o nome do produto que eu desejo, camera
+        homePage.insereNomeCampoPesquisa("Camera");
+
+        //Clica no botão pesquisar
+        homePage.clicaBotaoPesquisar();
+
+        String tituloPaginaEsperada = "https://automacao.testerglobal.com/product/camera/";
+        String paginaEncontrada = driver.getCurrentUrl();
+
+        assertEquals (tituloPaginaEsperada, paginaEncontrada);
+    }
+
+    // metodo de preparação, roda tudo que for antes do teste.
     @BeforeAll
     public void setup(){
+
+        // instanciando o chromedriver
         driver = new ChromeDriver();
+        // definindo um tempo de espera
         driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10));
 
     }
 
-    //Método para limpeza, tudo que irá rodar depois dos testes.
+    // metodo para limpeza, roda tudo o que for depois do teste.
     @AfterAll
     public void tearDown () {
         driver.close();
