3d8bb2ac6a00ea2a4731f4cc1cd8a366eb15ad0c
fabiodrneles <fabiodrneles@gmail.com>
2024-11-29 12:03:03 -0300

    mapeando os elementos

--- src/test/java/Home.java
+++ src/test/java/Home.java
@@ -1,4 +1,12 @@
+import org.openqa.selenium.By;
+
 public class Home {
+
+    // Campo de pesquisa
+    By campoPesquisa = By.cssSelector("#blog > div.page-wrap > div.site-header.container-fluid.woo-heading > div > div > div.header-search-widget > div.head-form.hidden-xs > div > form > input.header-search-input");
+
+    // Botão Pesquisa
+    By botaoPesquisa = By.cssSelector("#blog > div.page-wrap > div.site-header.container-fluid.woo-heading > div > div > div.header-search-widget > div.head-form.hidden-xs > div > form > button");
 
 
 }
